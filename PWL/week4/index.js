93ec1705ee8a6943cde55403b54d54b73aa869ba
emanuellzoe <emanuellzoee@gmail.com>
2025-09-29 16:02:25 +0700

    commit

--- PWL/week4/index.js
+++ PWL/week4/index.js
@@ -29,32 +29,64 @@
 //(boolean expression) ? true : false
 //DATA
 
-const today = {
-    day: 1,
-    name: "Monday"
+// const today = {
+//     day: 1,
+//     name: "Monday"
+// }
+
+// const harilain = {
+//     day: 5,
+//     name: "Friday"
+// }
+
+// //Ternary
+// new Date().getDay() === today.day ? (console.log(`Today is ${today.name}`)) : (console.log ("NotToday"))
+
+// //Ternary single condition
+// new Date().getDay() === today.day
+// ? (console.log(`Today is ${today.name}`))
+// : (console.log (`Today is not ${today.name} and not ${harilain.name}`))
+
+// //Multiple condition
+// new Date().getDay() === today.day
+// ? (console.log(`Today is ${today.name}`))
+// : new Date().getDay() === harilain.day
+//     ? (console.log(`Today is ${harilain.name}`))
+//     : (console.log (`Today is not ${today.name} and not ${harilain.name}`))
+
+
+
+//3
+// perbedaan if else dan switch case
+// if else akan mengecek kondisi satu persatu
+// switch case akan langsung menuju ke kondisi yang sesuai
+// switch case lebih efisien jika banyak kondisi
+
+//contoh switch case
+const day = () => {
+    switch (new Date().getDay()) {
+        case 0:
+            return "Sunday";
+            break;
+        case 1:
+            return "Monday";
+            break;
+        case 2:
+            return "Tuesday";
+            break;
+        case 3:
+            return "Wednesday";
+            break;
+        case 4:
+            return "Thursday";
+            break;
+        case 5:
+            return "Friday";
+            break;
+        case 6:
+            return "Saturday";
+            break;
+    }
 }
-
-const harilain = {
-    day: 5,
-    name: "Friday"
-}
-
-//Ternary
-new Date().getDay() === today.day ? (console.log(`Today is ${today.name}`)) : (console.log ("NotToday"))
-
-//Ternary single condition
-new Date().getDay() === today.day
-? (console.log(`Today is ${today.name}`))
-: (console.log (`Today is not ${today.name} and not ${harilain.name}`))
-
-//Multiple condition
-new Date().getDay() === today.day
-? (console.log(`Today is ${today.name}`))
-: new Date().getDay() === harilain.day
-    ? (console.log(`Today is ${harilain.name}`))
-    : (console.log (`Today is not ${today.name} and not ${harilain.name}`))
-
-    
-
-
+console.log(`Today is ${day()}`);
 
