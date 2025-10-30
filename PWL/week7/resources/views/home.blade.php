1a7ac0ec176fa120984c64c63f9f728c53a3c762
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-10-30 22:36:57 +0700

    update user icon

--- PWL/week7/resources/views/home.blade.php
+++ PWL/week7/resources/views/home.blade.php
@@ -5,22 +5,24 @@
   <title>Home</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
 </head>
 
 <body>
 
-  <div class="container-fluid">
+  <div class=" container-fluid">
     <!-- Baris Pertama HEADER -->
     <div class="row">
       <div class="col-lg-12 py-4 bg-primary">
-        <div class="btn-group">
-          <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Right-aligned menu
+        <div class="dropdown float-right">
+          <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown">
+            <i class="bi bi-person-fill"></i> User
           </button>
-          <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item" type="button">Action</button>
-            <button class="dropdown-item" type="button">Another action</button>
-            <button class="dropdown-item" type="button">Something else here</button>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+            <button class="dropdown-item" type="#">Action</button>
+            <button class="dropdown-item" type="#">Another action</button>
+            <button class="dropdown-item" type="#">Something else here</button>
           </div>
         </div>
       </div>
