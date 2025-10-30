affe700e951a4e8af4ec59b6b0392ce6a4c55de6
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-10-30 23:55:44 +0700

    commit selese

--- PWL/week7/app/Http/Controllers/PageController.php
+++ PWL/week7/app/Http/Controllers/PageController.php
@@ -2,23 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+
 
 //2
 class PageController extends Controller
 {
     public function home()
     {
-        return view('home');
+        return view('home', ['key' => 'home']);
     }
 
     public function movie()
     {
-        return view('movie');
+        return view('movie', ['key' => 'movie']);
     }
 
     public function genre()
     {
-        return view('genre');
+        return view('genre', ['key' => 'genre']);
     }
 }
--- PWL/week7/resources/views/layouts/main.blade.php
+++ PWL/week7/resources/views/layouts/main.blade.php
@@ -2,21 +2,23 @@
 <html lang="en">
 
 <head>
-    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
+    <title>@yield('title')</title>
 </head>
 
 <body>
 
-    <div class=" container-fluid">
+    <div class="container-fluid">
         <!-- Baris Pertama HEADER -->
         <div class="row">
-            <div class="col-lg-12 py-4 bg-primary">
+            <div class="col-lg-12 py-2 bg-primary">
                 <div class="dropdown float-right">
-                    <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="bi bi-person-fill"></i> User
                     </button>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
@@ -42,17 +44,25 @@
         <div class="row">
             <div class="col-md-2 border vh-100">
                 <div class="nav flex-column nav-pills mt-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                    <a class="nav-link {{ ($key == 'home') ? 'active': ''}}"  href="/" role="tab">Home</a>
+                    <a class="nav-link {{ ($key == 'movie') ? 'active': ''}}"  href="/movie" role="tab">Movie</a>
+                    <a class="nav-link {{ ($key == 'genre') ? 'active': ''}}"  href="/genre" role="tab">Genre</a>
+                    <a class="nav-link" href="/bioskop" role="tab">Bioskop</a>
                 </div>
             </div>
+        
+    
+            <div class="col-md-10  vh-100">
+                    <div class="card mt-3">
+                        <div class="row">
+                            <div class="card-header"></div>
+                            <div class="card-body">
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
-    </div>
-    <div class="col-md-9 border vh-100">body kiri</div>
-    </div>
-
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
