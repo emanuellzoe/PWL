8d7cf43761134e49bb0498a535b753d7cc6bb960
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-10-30 23:01:45 +0700

    update navigasi bar

--- PWL/week7/resources/views/home.blade.php
+++ PWL/week7/resources/views/home.blade.php
@@ -40,9 +40,18 @@
     </div>
     <!-- Akhir Baris Pertama HEADER -->
     <div class="row">
-      <div class="col-md-3 border vh-100">body kiri</div>
-      <div class="col-md-9 border vh-100">body kiri</div>
+      <div class="col-md-2 border vh-100">
+        <div class="nav flex-column nav-pills mt-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+          <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+          <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+          <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="col-md-9 border vh-100">body kiri</div>
+  </div>
 
   </div>
 
