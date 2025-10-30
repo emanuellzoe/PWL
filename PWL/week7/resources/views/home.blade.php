7738bb14c3fe6e28c2ff3ec7760dbf79a25de2a0
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-10-30 22:26:36 +0700

    commit

--- PWL/week7/resources/views/home.blade.php
+++ PWL/week7/resources/views/home.blade.php
@@ -1,21 +1,37 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Home</title>
-  </head>
-  <body>
-    
-    <div class="container-fluid">
+
+<head>
+  <title>Home</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container-fluid">
     <!-- Baris Pertama HEADER -->
     <div class="row">
-        <div class="col-lg-12 py-2">Header</div>
-    </div>
-      <!-- Akhir Baris Pertama HEADER -->
-        <div class="row">
-            <div class="col-md-3 border vh-100">body kiri</div>
-            <div class="col-md-9 border vh-100">body kiri</div>
+      <div class="col-lg-12 py-4 bg-primary">
+        <div class="btn-group">
+          <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Right-aligned menu
+          </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item" type="button">Action</button>
+            <button class="dropdown-item" type="button">Another action</button>
+            <button class="dropdown-item" type="button">Something else here</button>
+          </div>
         </div>
-
+      </div>
     </div>
+    <!-- Akhir Baris Pertama HEADER -->
+    <div class="row">
+      <div class="col-md-3 border vh-100">body kiri</div>
+      <div class="col-md-9 border vh-100">body kiri</div>
+    </div>
+
+  </div>
 </body>
+
 </html>
