8c44198fbbf3a62a197e53bbc5ab5d0754affb6a
emanuellzoe <emanuellzoee@gmail.com>
2025-11-03 15:06:27 +0700

    update pada table movie

--- PWL/week8/resources/views/movie.blade.php
+++ PWL/week8/resources/views/movie.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.main')
 @section('title', 'Movie') 
 @section('content')
-    <h3>MOVIE</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptatibus!</p>
+    <div class="card">
+        <div class="card-header"></div>
+        <div class="card-body">
+            <table id="example" class="display" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>ImDB</th>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Year</th>
+                        <th>Poster</th>
+                    </tr>
+                </thead>
+                <body>
+                    <tr>
+                        <td>1</td>
+                        <td>tt098765</td>
+                        <td>Abdadi nan Jaya</td>
+                        <td>Horor</td>
+                        <td>2025</td>
+                        <td>image.jpg</td>
+                    </tr>
+                </body>
+            </table>
+        </div>
+    </div>
 @endsection
