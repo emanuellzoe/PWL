0e60e9d6249389b0250f128a9639209c891ca1b7
emanuellzoe <emanuellzoee@gmail.com>
2025-11-03 14:55:58 +0700

    commit

---
+++ PWL/week8/app/Movie.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $table = 'movie';
+    protected $fillable = ['title', 'director', 'release_year', 'genre'];
+}
