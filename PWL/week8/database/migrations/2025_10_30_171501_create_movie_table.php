58123f33e30dbc1b60c9b6061d37dc033442bf6e
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-11-05 23:54:31 +0700

    commit

--- PWL/week8/database/migrations/2025_10_30_171501_create_movie_table.php
+++ PWL/week8/database/migrations/2025_10_30_171501_create_movie_table.php
@@ -14,16 +14,7 @@ class CreateMovieTable extends Migration
      */
     public function up()
     {
-        Schema::create('movie', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('imdb', 100)->unique();
-            $table->string('title', 30);
-            $table->string('genre', 20);
-            $table->year('year');
-            $table->text('description');
-            $table->string('cover', 150);
-            $table->timestamps();
-        });
+        Schema::create('movie', function (Blueprint $table) {});
     }
 
     /**
