803e9706a79e5f1c792f36e876b3b7afb7ae9cb8
emanuellzoe <emanuellzoee@gmail.com>
2025-11-10 15:09:14 +0700

    template week9

---
+++ PWL/week9/app/Http/Controllers/PageController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+
+//2
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('home', ['key' => 'home']);
+    }
+
+    public function movie()
+    {
+        $movie = Movie::orderBy('id', 'desc')->get();
+        return view('movie', ['key' => 'movie', 'mv' => $movie]);
+    }
+
+    public function genre()
+    {
+        return view('genre', ['key' => 'genre']);
+    }
+    public function movieaddform()
+    {
+        return view('movieaddform', ['key' => 'movie']);
+    }
+
+    public function moviesave(Request $request)
+    {
+        if($request->hasFile('cover'))
+        {
+            $file_name = time().'-'.$request->file('cover')->getClientOriginalName();
+            $path = $request->file('cover')->storeAs('cover', $file_name,'public');
+        } else
+        {
+            $file_name = null;
+            $path = null;
+        }
+        Movie::create([
+            'imdb' => $request->imdb,
+            'title' => $request->title,
+            'genre' => $request->genre,
+            'year' => $request->year,
+            'description' => $request->description,
+            'cover' => $file_name
+        ]);
+        return redirect('/movie');
+    }
+
+}
---
+++ PWL/week9/resources/views/movie.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+@section('title', 'Movie') 
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <a href="/movie/addform" class="btn btn-primary"><i class="bi bi-plus-square"></i></a>
+        </div>
+        <div class="card-body">
+            <table id="example" class="display" style="width: 100%" >
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>ImDB</th>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Year</th>
+                        <th>Cover</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($mv as $idx => $m)
+                    <tr>
+                        <td>{{ $idx + 1 }}</td>
+                        <td>{{ $m->imdb }}</td>
+                        <td>{{ $m->title }}</td>
+                        <td>{{ $m->genre }}</td>
+                        <td>{{ $m->year }}</td>
+                        <td>
+                            @if ($m->cover)
+                                <img src="{{ asset('/storage/cover/'.$m->cover) }}" alt="{{ $m->cover }}" width="80" height="80">
+                            @else
+                                <img src="{{ asset('storage/cover/no-image.jpg') }}" alt="No Image" width="80" height="80">
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
