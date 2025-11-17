513ad59646e637ab72882638f6c9916f3344e201
emanuellzoe <emanuellzoee@gmail.com>
2025-11-17 15:46:50 +0700

    penambahan alert movie

--- PWL/week10/resources/views/movie.blade.php
+++ PWL/week10/resources/views/movie.blade.php
@@ -6,10 +6,12 @@
             <a href="/movie/addform" class="btn btn-primary"><i class="bi bi-plus-square"></i></a>
         </div>
         <div class="card-body">
-            @if (session('alert'))
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>{{ session('alert') }}</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
             <table id="example" class="display" style="width: 100%" >
                 <thead>
                     <tr>
