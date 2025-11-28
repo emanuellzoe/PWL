6660eb7fc8fc1fcedb79f5e02e2a9a8274d641aa
emanuellzoe <emanuellzoee@gmail.com>
2025-11-28 21:23:04 +0700

    penambahan login

--- PWL/week10/routes/web.php
+++ PWL/week10/routes/web.php
@@ -25,3 +25,4 @@ Route::get('/users','PageController@users');
 Route::get('/users/addform', 'PageController@usersaddform');
 Route::post('/users/save', 'PageController@userssave');
 Route::get('/users/deleteform/{id}', 'PageController@usersdelete');
+Route::get('/login','AuthController@loginform');
