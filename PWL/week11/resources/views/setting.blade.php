c5ffcaa4eea20a1ddc2c2c4db6c98aa543892d0c
emanuellzoe <emanuellzoee@gmail.com>
2025-11-30 21:36:19 +0700

    membuat setting.blade.php

---
+++ PWL/week11/resources/views/setting.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.main')
+
+@section('title', 'User')
+
+@section('content')
+<div class="card">
+    <div class="card-header"></div>
+
+    <div class="card-body">
+        <form action="/updatepass" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <label for="password_lama">Password Lama</label>
+                <input type="text" name="password_lama" class="form-control" id="password_lama" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password_baru">Password Baru</label>
+                <input type="text" name="password_baru" class="form-control" id="password_baru" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password_konfirmasi">Konfirmasi Password Baru</label>
+                <input type="text" name="password_konfirmasi" class="form-control" id="password_konfirmasi" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+    </div>
+</div>
+@endsection
