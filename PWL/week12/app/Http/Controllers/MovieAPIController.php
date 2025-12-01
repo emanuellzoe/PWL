d1fd166b87e813d06e6614fb4bfd9bdb2e254c2a
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-12-02 00:06:56 +0700

    perubahan

--- PWL/week12/app/Http/Controllers/MovieAPIController.php
+++ PWL/week12/app/Http/Controllers/MovieAPIController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieAPIController extends Controller
 {
     //
+    public function apimovies()
+    {
+        $movie = Movie::orderby("id", "desc")->get();
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Berhasil ditampilkan',
+                'data' => $movie
+            ],
+            200
+        );
+    }
 }
